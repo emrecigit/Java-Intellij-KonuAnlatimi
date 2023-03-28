ddfaae480ab98bb60fd9dc1695ecddf44c771c97
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-03-28 22:05:40 +0300

    2303282205

--- src/day09_pratice/Soru.java
+++ src/day09_pratice/Soru.java
@@ -2,11 +2,25 @@ package day09_pratice;
 
 public class Soru {
     public static void main(String[] args) {
-        /*// Kullanicidan istedigi kadar sayi alip,
+
+
+   /*// Kullanicidan istedigi kadar sayi alip,
         bu sayilari bir Array'de toplayip
         bu sayilarin toplamini yazdiran bir program yaziniz
          */
+    }
 
-
+    public static class String {
     }
 }
+
+
+
+
+
+
+
+
+
+
+
